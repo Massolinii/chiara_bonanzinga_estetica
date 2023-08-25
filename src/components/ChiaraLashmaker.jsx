53a465117cc2b34ed05f5b585285dd1d4f5e5eb7
Massolinii <massimilianoesposito548@gmail.com>
2023-08-25 17:56:02 +0200

    Made Hero (text to modify)

---
+++ src/components/ChiaraLashmaker.jsx
@@ -0,0 +1,26 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function ChiaraLashmaker() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="col-12 col-md-9 col-lg-7 ps-3 pt-3 hero">
+            <h1 className="chi-bon-est">CHIARA BONANZINGA ESTETICA</h1>
+            <p className="ps-3 pt-4 chi-sono">
+              Il luogo ideale per prenderti cura del tuo sguardo e della tua
+              bellezza. Situato in Via Palermo (ME), lasciati stupire dai miei
+              trattamenti One to One, Wet, Volume e Megavolume, dedicati ad
+              esaltare la bellezza dei tuoi occhi. Con una tecnica raffinata ed
+              un utilizzo di prodotti di alta qualità, sono garantiti risultati
+              duraturi e soddisfacenti. <br /> <br />
+              Non esitare a prenotare un appuntamento per scoprire come
+              raggiungere la tua bellezza ideale delle ciglia e sopracciglia. Ti
+              aspetto!
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
---
+++ src/components/PerInfo.jsx
@@ -0,0 +1,10 @@
+export default function PerInfo() {
+  return (
+    <p className="m-0 p-2">
+      Per info e prenotazioni:{" "}
+      <a href="tel:3479844653" className="phone-number">
+        (+39) 3479844653
+      </a>
+    </p>
+  );
+}
