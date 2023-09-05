2db7a688775c8b57717d6793400fcb350206206c
Massolinii <massimilianoesposito548@gmail.com>
2023-09-05 15:56:47 +0200

    V.0 done

--- src/components/Contatti.jsx
+++ src/components/Contatti.jsx
@@ -2,12 +2,14 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export default function Contatti() {
   return (
-    <Container fluid className="container-section py-4">
-      <Row className="justify-content-center">
+    <Container fluid className="container-section py-4" id="contatti">
+      <Row className="justify-content-center hero-text">
         <Col xs={11} md={9} lg={7} className="p-3 px-4 full-width-link">
           <p>
-            Puoi raggiungerci al numero:
-            <a href="tel:+39123456789"> 3479844653</a>
+            Puoi prenotare un appuntamento al numero:{" "}
+            <a href="tel:+39123456789" className="text-black">
+              3479844653
+            </a>
           </p>
         </Col>
       </Row>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,18 @@ import ChiaraLashmaker from "./ChiaraLashmaker";
 import ScopriServizi from "./ScopriTrattamenti";
 import { Container } from "react-bootstrap";
 import Contatti from "./Contatti";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const id = hash.replace("#", "");
+      const element = document.getElementById(id);
+      if (element) element.scrollIntoView();
+    }
+  }, []);
+
   return (
     <div>
       <Container fluid className="container-page">
